15a3c9eeba4d3eaf601fbbc466a84a5281f458a9
kevinelong <kevinelong@gmail.com>
2024-10-25 09:17:32 -0700

    updated

---
+++ scripts/act.js
@@ -0,0 +1,33 @@
+
+document.addEventListener("DOMContentLoaded", ()=>{
+
+    
+    for(c of categories){
+        // categorySelect.innerHTML += `<option>${c}</option>`; 
+        // += append/concatenate (not replace)      
+        categorySelect.appendChild(new Option(c));
+    }
+
+    categorySelect.addEventListener("change", ()=>{
+        activitySelect.innerHTML = "";
+        activitySelect.appendChild(new Option("Select an Activity"));
+        for(a of activities){
+            if(a.category === categorySelect.value){
+                activitySelect.appendChild(new Option(a.name));
+            }
+        }
+    });
+
+    activitySelect.addEventListener("change", ()=>{
+        for(a of activities){
+            if(a.name === activitySelect.value){
+                for(key in a){
+                    detailTable.innerHTML += `
+                        <tr><th> ${key} </th><td> ${a[key]} </td></tr>
+                    `;
+                }
+            }
+        }
+    });
+
+}); // end content loaded
