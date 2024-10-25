2f211ef72a08b4cbd5ef1117946a0b373f44e51e
kevinelong <kevinelong@gmail.com>
2024-10-25 09:18:23 -0700

    pretty

--- scripts/act.js
+++ scripts/act.js
@@ -1,27 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
 
-document.addEventListener("DOMContentLoaded", ()=>{
-
-    
-    for(c of categories){
+    for (c of categories) {
         // categorySelect.innerHTML += `<option>${c}</option>`; 
         // += append/concatenate (not replace)      
         categorySelect.appendChild(new Option(c));
     }
 
-    categorySelect.addEventListener("change", ()=>{
+    categorySelect.addEventListener("change", () => {
         activitySelect.innerHTML = "";
         activitySelect.appendChild(new Option("Select an Activity"));
-        for(a of activities){
-            if(a.category === categorySelect.value){
+        for (a of activities) {
+            if (a.category === categorySelect.value) {
                 activitySelect.appendChild(new Option(a.name));
             }
         }
     });
 
-    activitySelect.addEventListener("change", ()=>{
-        for(a of activities){
-            if(a.name === activitySelect.value){
-                for(key in a){
+    activitySelect.addEventListener("change", () => {
+        for (a of activities) {
+            if (a.name === activitySelect.value) {
+                for (key in a) {
                     detailTable.innerHTML += `
                         <tr><th> ${key} </th><td> ${a[key]} </td></tr>
                     `;
