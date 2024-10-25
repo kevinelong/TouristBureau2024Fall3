38b28d22e85f318e54da1e19898409fab965116a
kevinelong <kevinelong@gmail.com>
2024-10-25 08:19:33 -0700

    t3

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+const nav = `
+    <nav>
+        <a href="./index.html"> Home </a>
+        <a href="./activities.html"> Activities </a>
+        <a href="./hikes.html"> Hikes </a>
+        <a href="./video.html"> Video </a>
+    </nav>
+`;
